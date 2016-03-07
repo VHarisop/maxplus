56bc31e85cf62a305c322626ad3426d52b5772f3
VHarisop <the.machine.headed@gmail.com>
2016-03-07 17:14:45 +0200

    Rearranged rows/columns

--- mp_principal_sol.m
+++ mp_principal_sol.m
@@ -1,16 +1,25 @@
-function sol = mp_principal_sol(A, b)
+function S = mp_principal_sol(A, B)
 	## MP_SOLVE Calculates the principal solution to a one-sided
 	## system in max-algebra.
 	##
-	## Usage: s = mp_principal_sol(A, b);
+	## Usage: S = mp_principal_sol(A, B);
 	##
 	## The solution of a one-sided system,
-	## 		A (o) x = b
+	## 		A (o) X = B
 	## in max algebra, is not guaranteed to exist. However, if a
 	## solution exists, it is given by the principal solution
-	##		x_optim = (-A') (o)' b
+	##		X_optim = (-A') (o)' B
 	## where (o)' is the dual operator of (o). With (o)', the
 	## minimum replaces the maximum and addition is retained.
 
-	sol = min(-A' + b, [], 2);
+	sz_a = size(A);
+	sz_b = size(B);
+	# size of A should be m x n, size of B should be m x k
+	if (sz_a(1) != sz_b(1))
+		error("Matrix sizes are incompatible!");
+	end
+
+	for i = 1:sz_b(2)
+		S(:, i) = min(-A' + B(:, i)', [], 2);
+	endfor
 endfunction
--- mp_times.m
+++ mp_times.m
@@ -1,23 +1,30 @@
 function R = mp_times (A, B)
 	## MP_TIMES Performs matrix 'multiplication' in max-plus algebra.
+	## Matrix multiplication is performed by summing each row with a
+	## column, and taking the maximum value of that summation.
+	##
+	## In greater detail: C = A x B means that
+	## 		c_{ij} = max_{1 .. K}{ a_{ik} + b_{kj} }
 	## 
 	## Usage: R = mp_times(A, B);
 	##
 	## Here, A is assumed to be a matrix (or vector) of size n x k and B
-	## a matrix (or vector) of size m x k. The resulting matrix is of
-	## size m x n.
+	## a matrix (or vector) of size k x m. The resulting matrix is of
+	## size n x m.
 
 	[bx, by] = size(B);
 	[ax, ay] = size(A);
 
 	## Make sure that matrix sizes are compatible in order
 	## to perform matrix multiplication
-	assert(by == ay, "Matrix sizes are incompatible!");
+	if (bx != ay)
+		error("Matrix sizes are incompatible!");
+	endif
 
-	R = zeros(bx, ax);
+	R = zeros(ax, by);
 	## max-plus matrix multiplication is performed by taking the maximum
 	## over every (matrix row + vector_column)
-	for i = 1:bx
-		R(i, :) = max(A + B(i, :), [], 2);
+	for i = 1:by
+		R(:, i) = max(A + B(:, i)', [], 2);
 	endfor
 endfunction
