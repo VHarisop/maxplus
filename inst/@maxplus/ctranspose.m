77e1f5f479c30a6d94b9da1f7c829d17301fb6d8
VHarisop <the.machine.headed@gmail.com>
2016-03-07 19:18:54 +0200

    Fixed off-by-one error

--- inst/@maxplus/ctranspose.m
+++ inst/@maxplus/ctranspose.m
@@ -1,4 +1,4 @@
-function R = transpose(M)
+function R = ctranspose(M)
 	## Return the conjugate transpose of M in the max plus semiring.
 	## The complex conjugate transpose is defined as -M' in
 	## conventional notation.
