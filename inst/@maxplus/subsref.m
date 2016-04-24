f801aa14b593cdd419020a05c484435c6c0b71a2
VHarisop <the.machine.headed@gmail.com>
2016-04-24 03:38:37 +0300

    Added indexing in subsref.m

--- inst/@maxplus/subsref.m
+++ inst/@maxplus/subsref.m
@@ -1,6 +1,12 @@
 function r = subsref (val, idx)
-	## Retrieve the .mat field of a @maxplus object.
-	if (strcmp (idx.type, ".") && strcmp (idx.subs, 'mat'))
+	## Overloads subsref function for maxplus objects.
+
+	# Retrieve the .mat field of a @maxplus object.
+	if strcmp(idx.type, ".") && strcmp(idx.subs, 'mat')
 		r = val.mat;
+	# if indexing is requested, pass that request "downwards"
+	# and return a maxplus object from the result, if any
+	elseif strcmp(idx.type, '()')
+		r = maxplus(subsref(val.mat, idx));
 	endif
 endfunction
